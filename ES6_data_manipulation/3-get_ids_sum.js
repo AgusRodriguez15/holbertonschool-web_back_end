b426c1d4782b65aec5d8e3532235a2f3f1778b77
AgusRodriguez15 <8670@holbertonstudents.com>
2024-07-25 02:29:48 -0300

    sum all ids

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+export default function getStudentIdsSum(students) {
+  const contador = 0;
+  const sumContador = students.reduce((accumulator, currentValue) => accumulator + currentValue.id,
+    contador);
+  return sumContador;
+}
