ad913cfe8b681559d6e4c7a40cc84dfc7bb18cea
AgusRodriguez15 <8670@holbertonstudents.com>
2024-08-14 11:41:13 -0300

    fixed pycodestyle

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,10 +5,10 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    
+
     """function make_multiplier"""
-    
+
     def mul(i: float) -> float:
         return i * multiplier
-    
+
     return mul
