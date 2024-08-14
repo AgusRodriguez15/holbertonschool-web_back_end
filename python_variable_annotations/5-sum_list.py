35ca74e4e7cc2a53683aac73345deb1ced597d7f
AgusRodriguez15 <8670@holbertonstudents.com>
2024-08-14 09:54:20 -0300

    add typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """solution"""
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """function sum_list"""
     sum = 0
     for i in input_list:
