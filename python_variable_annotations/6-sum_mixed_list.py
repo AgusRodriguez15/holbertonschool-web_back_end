8f956bca65953f3c88d2d404c8f404419d539de0
AgusRodriguez15 <8670@holbertonstudents.com>
2024-08-14 10:01:43 -0300

    change the order

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """function sum_mixed_list"""
     sum = 0
     for i in mxd_lst:
