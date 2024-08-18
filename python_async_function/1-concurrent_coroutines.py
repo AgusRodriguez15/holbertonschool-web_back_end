32367933aac8df0bf52732258db224cef3e02290
AgusRodriguez15 <8670@holbertonstudents.com>
2024-08-18 17:25:34 -0300

    3 solutioion

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""solution"""
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> list:
+    """function async wain_n"""
+    
+    
+     
