7cd6cf55abf1b7b0ec639856352a3cd890f7d7ef
AgusRodriguez15 <8670@holbertonstudents.com>
2024-08-18 21:03:45 -0300

    1 solutiion

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """solution"""
 
+import asyncio
+import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> list:
+async def wait_n(n: int, max_delay: int = 10) -> typing.List[float]:
     """function async wain_n"""
-    
-    
-     
+    delays = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+    return sorted(delays)
