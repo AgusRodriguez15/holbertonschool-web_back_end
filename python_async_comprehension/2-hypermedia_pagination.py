59dfd25392f6207708bf2380ebad7e2a760dcb5b
AgusRodriguez15 <8670@holbertonstudents.com>
2024-08-25 20:42:10 -0300

    solution 2

--- python_async_comprehension/2-hypermedia_pagination.py
+++ python_async_comprehension/2-hypermedia_pagination.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-"""aolution"""
+"""solution"""
 import asyncio
 import time
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async_comp = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """measure_runtime"""
-    tasks = []
+    """measure runtime"""
     start_time = time.time()
-    for i in range(4):
-        tasks.append(asyncio.create_task(async_comprehension()))
+
+    tasks = [async_comp() for i in range(4)]
+
     await asyncio.gather(*tasks)
+
     end_time = time.time()
+
     return end_time - start_time
