20d9d5b8b9eea347248eeb8163824c11b5012e84
AgusRodriguez15 <8670@holbertonstudents.com>
2024-08-25 20:02:09 -0300

    fixed

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """solution"""
-import pymogo
+import pymongo
 
 def list_all(mongo_collection):
     """list_all"""
     if not mongo_collection:
         return []
-    else:
-        return list(mongo_collection.find())
+    return list(mongo_collection.find())
